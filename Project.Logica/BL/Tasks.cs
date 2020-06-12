e15963b1bfdc7ec101f49f40384d6cbbf5da0796
Alex Solarte <alexadrian1990@gmail.com>
2020-06-11 19:23:59 -0500

    projectcore

---
+++ Project.Logica/BL/Tasks.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectCore.Logica.BL
+{
+    public class Tasks
+    {
+        public List<Models.DB.Tasks> GetTasks(int? projectId, int? id)
+        {
+            DAL.Models.ProjectCoreContext _context = new DAL.Models.ProjectCoreContext();
+
+            var listTasks = (from _tasks in _context.Tasks 
+                join _states in _context.States on _tasks.StateId equals _state.Id
+                join activities in _context.Activities on _tasks.ActivityId equals _activities.Id
+                join _priorities in _context.Priorities on _tasks.PriorityId equals _priorities.Id
+                select new Models.DB.Tasks
+                {
+                    Id = _tasks.Id,
+                    Title = _tasks.Title,
+                    Details = _tasks.Details,
+                    ExpirationDate = _tasks.ExpirationDate,
+                    IsCompleted = _tasks.IsCompleted,
+                    Effort = _tasks.Effort,
+                    RemainingWork = _tasks.RemainingWork,
+                    StateId = _tasks.StateId,
+                    States = new Models.DB.States
+
+                    {
+                        Name = _states.Name
+                    },
+
+                    PriorityId = _tasks.PriorityId,
+                    Priorities = new Models.DB.Priorities
+                    {
+                        Name = _priorities.Name
+                    },
+
+                    ActivityId = _tasks.ActivityId,
+                    Activities = new Models.DB.Activities
+                    {
+                        Name = _activities.Name
+                    },
+
+                    ProjectId = _tasks.ProjectId
+
+                }).ToList();
+
+            if (projectId != null)
+                listTasks = listTasks.Where(x => x.ProjectId == projectId).ToList();
+            if (id != null)
+                listTasks = listTasks.Where(x => x.Id == id).ToList();
+            return listTasks;
+
+        }
+
+    }
+}
---
+++ ProjectCore/Controllers/TasksController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectCore.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult Index(int? projectId)
+        {
+
+            Logica.BL.Tasks tasks = new Logica.BL.Tasks();
+
+
+
+            return View();
+        }
+    }
+}
